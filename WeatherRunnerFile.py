e7c9f7d8bf7ba1706e739f7521e47e199cb0c8c0
Harlan Howe <harlan.howe@kinkaid.org>
2024-07-23 20:03:13 -0500

    Created WeatherRunnerFile. Currently reads data file and asks user for a day's weather info (no safety checks).

---
+++ WeatherRunnerFile.py
@@ -0,0 +1,44 @@
+import csv
+from typing import Dict, Tuple
+
+from SplitterNodeFile import SplitterNode
+
+
+def main():
+    print("Weather Runner")
+
+    data_from_file = load_data("Weather Data 2014-2024.tsv")
+    print(f"Loaded {len(data_from_file)} days.")
+
+    root = SplitterNode(0)
+    root.build_subtree(list(data_from_file.keys()))
+
+    # print(root)
+
+    search_value = request_weather_day()
+    print(search_value)
+
+def load_data(filename: str) -> Dict[Tuple[float, float, float, float], str]:
+    result = {}
+    tsv_file = open(filename)
+    read_tsv = csv.reader(tsv_file, delimiter="\t")
+
+    for day in read_tsv:
+        try:
+            datum = (float(day[0]), float(day[1]), float(day[2]), float(day[3]))
+            description = day[4]
+            result[datum] = description
+        except ValueError:
+            print(f"skipping line: {day}.")
+    return result
+
+def request_weather_day() -> Tuple[float, float, float, float]:
+    print("Please enter information about a day's weather:")
+    precip = input("How much precipitation, in inches? ")
+    max = input("Max temperature (°F)? ")
+    min = input("Min temperature (°F)? ")
+    day = input("Day of year (0-366)? ")
+    return (float(precip), float(max), float(min), float(day))
+
+if __name__ == "__main__":
+    main()
