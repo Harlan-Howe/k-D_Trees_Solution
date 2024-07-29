1c026d17f5ca431455cba3665d22b367393617ef
Harlan Howe <harlan.howe@kinkaid.org>
2024-07-29 11:18:36 -0500

    temporarily disabled printing the parameters - too much noise.

--- KinkaidDecorators.py
+++ KinkaidDecorators.py
@@ -9,7 +9,7 @@ def log_start_stop_method(func: Callable[..., Any]) -> Callable[..., Any]:
     @functools.wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
         indent = "\t"*max(0, len(traceback.extract_stack())-1)
-        logging.info(f"{indent}Starting method {func.__name__}({args})")
+        logging.info(f"{indent}Starting method {func.__name__}")#({args})")
         value = func(*args, **kwargs)
         logging.info(f"{indent}Finishing method: {func.__name__}")
         return value
