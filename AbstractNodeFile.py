dbe715f2f82ccaecabe5a7cc880d01739ae259ab
Harlan Howe <harlan.howe@kinkaid.org>
2024-07-29 21:40:45 -0500

    Improved documentation

--- AbstractNodeFile.py
+++ AbstractNodeFile.py
@@ -21,6 +21,11 @@ class AbstractNode(ABC):  # ABC means this is an abstract class
 
     @abstractmethod
     def recursive_to_string(self, depth: int = 0) -> str:
+        """
+        creates a string that represents this tree, with data values indented to make it appear tree-like
+        :param depth: the number of tabs we should add on the front end of each line of this string
+        :return: the descriptive string, which may wind up incorporated in another string, as this is recursive.
+        """
         pass
 
     @abstractmethod
@@ -29,10 +34,20 @@ class AbstractNode(ABC):  # ABC means this is an abstract class
                      best_value_so_far: Optional[Tuple[float, ...]],
                      best_distance_so_far: float,
                      visualizer=None) -> Tuple[Optional[Tuple[float, ...]], Optional[float]]:
+        """
+        finds the nearest datum to the given target, presumably better than the best_value_so_far, beating the
+        best_distance_so_far. rRturns the datum and distance of a better point
+        :param target:  the tuple of floats for which we are trying to find the closest tuple of points in our data set.
+        :param best_value_so_far: the closest point to target found so far in this search process, before this call
+        :param best_distance_so_far: the distance to the best_value_so_far.
+        :param visualizer: if this is not none, then this will show our search in progress.
+        :return: the (tuple of floats, distance to that tuple), if there is one better than the ones given as parameters;
+        otherwise, (None, None).
+        """
         pass
 
-    def __repr__(self):
+    def __repr__(self):  # equivalent to Java's toString()
         return self.recursive_to_string()
 
-    def __str__(self):
+    def __str__(self):  # equivalent to Java's toString()
         return self.recursive_to_string()
