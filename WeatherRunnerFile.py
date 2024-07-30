e11f5049296c3527fe5983dd202b3974e0f99817
Harlan Howe <harlan.howe@kinkaid.org>
2024-07-29 21:33:41 -0500

    Improved documentation; now loops for many weather days for target of search

--- WeatherRunnerFile.py
+++ WeatherRunnerFile.py
@@ -11,31 +11,38 @@ def main():
     print(f"Loaded {len(data_from_file)} days.")
 
     root = SplitterNode(0)
-    root.build_subtree(data_from_file.keys())
+    root.build_subtree(set(data_from_file.keys()))
 
     # print(root)
+    while True:
+        search_value = request_weather_day()
+        print(f"You're looking for {search_value=}")
 
-    search_value = request_weather_day()
-    print(search_value)
+        closest, distance = root.find_nearest(search_value, None, float('inf'))
 
-    closest, distance = root.find_nearest(search_value, None, float('inf'))
-    if closest is None:
-        print("There was a problem... Search says none.")
-    else:
-        print(f"{closest=}\t{distance=}\nDescription: {data_from_file[closest]}")
+        if closest is None:
+            print("There was a problem... Search says none.")
+        else:
+            print(f"{closest=}\t{distance=}\nDescription: {data_from_file[closest]}")
 
 def load_data(filename: str) -> Dict[Tuple[float, float, float, float], str]:
+    """
+    read the data from the tsv (tab-separated values) file and load it into a dictionary in the format:
+    (precipitation, max_temp, min_temp, day_of_year) --> Description
+    :param filename: the name of the file to open
+    :return: the dictionary of information found in that file.
+    """
     result = {}
     tsv_file = open(filename)
-    read_tsv = csv.reader(tsv_file, delimiter="\t")
+    read_tsv = csv.reader(tsv_file, delimiter="\t")  # note: you'll need to import "csv.py" package.
 
-    for day in read_tsv:
+    for day in read_tsv:   # going line-by-line through file.
         try:
             datum = (float(day[0]), float(day[1]), float(day[2]), float(day[3]))
             description = day[4]
             result[datum] = description
         except ValueError:
-            print(f"skipping line: {day}.")
+            print(f"skipping line: {day}.")  # this will happen for the column headers line, for instance.
     return result
 
 def request_weather_day() -> Tuple[float, float, float, float]:
